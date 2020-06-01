851a9809a78c460b8799188ec41879f279437ac2
Raul de Heer <raul@supertask.nl>
2020-06-01 12:33:16 +0200

    Add auth to report controller

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,16 @@ $router->group(['prefix' => '/auth'], function () use ($router) {
 });
 
 $router->get('/menu', 'MenuController@list');
-$router->get('/report', 'ReportController@create');
 
-$router->group(['prefix' => '/dish', 'middleware' => 'auth'], function () use ($router) {
-    $router->post('', 'DishController@create');
-    $router->get('{dish}', 'DishController@detail');
-    $router->patch('{dish}', 'DishController@update');
-    $router->delete('{dish}', 'DishController@delete');
+$router->group(['middleware' => 'auth'], function () use ($router) {
+    $router->group(['prefix' => '/dish', 'middleware' => 'auth'], function () use ($router) {
+        $router->post('', 'DishController@create');
+        $router->get('{dish}', 'DishController@detail');
+        $router->patch('{dish}', 'DishController@update');
+        $router->delete('{dish}', 'DishController@delete');
+    });
+
+    $router->get('/report', 'ReportController@create');
 });
+
+
