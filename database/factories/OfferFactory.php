19800de1ca6e513ba92671bb82baabfa1d8b5886
Raul de Heer <raul@supertask.nl>
2020-06-03 19:07:35 +0200

    Remove tax from offer

--- database/factories/OfferFactory.php
+++ database/factories/OfferFactory.php
@@ -10,7 +10,6 @@ $factory->define(Offer::class, function (Faker $faker) {
     return [
         'name' => $faker->realText('70'),
         'valid_until' => $faker->boolean(75) ? $faker->dateTimeBetween('now', '+1 month') : null,
-        'price' => $faker->randomFloat(2, 7.5, 50),
-        'tax' => 9
+        'price' => $faker->randomFloat(2, 7.5, 50)
     ];
 });
